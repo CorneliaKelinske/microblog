a950c03644fe4fd472e1cfdd8aa27b993b25ee91
CorneliaKelinske <corneliakelinske@gmail.com>
2020-08-07 11:19:47 -0700

    included a template

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,9 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username' : 'Connie'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    return render_template('index.html', title="Home", user=user)
 
