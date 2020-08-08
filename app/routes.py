535aa51721bc265629d09c480eb2ba9cf7ef918f
CorneliaKelinske <corneliakelinske@gmail.com>
2020-08-08 07:18:04 -0700

    added a base template

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,21 @@
 from flask import render_template
 from app import app
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username' : 'Connie'}
-    return render_template('index.html', title="Home", user=user)
+    user = {'username': 'Cornelia'}
+    posts = [
+        {
+            'author': {'username': 'Gary'},
+            'body': 'Another Day at the Lake'
 
+        },
+
+        {
+            'author': {'username': 'Julia'},
+            'body': 'Down to One Gear'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
