d755905e587d402234f3c07e03aae10481b6c468
CorneliaKelinske <corneliakelinske@gmail.com>
2020-11-02 11:08:43 -0800

    am now recording the last visit time for a user

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,7 @@ from flask_login import logout_user
 from flask_login import login_required
 from flask import request
 from werkzeug.urls import url_parse
+from datetime import datetime
 
 
 
@@ -75,3 +76,9 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
