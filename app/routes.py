8a4f71da0b71555c059f80ea5786aedd2b8cd272
Cornelia Kelinske <corneliakelinske@gmail.com>
2020-12-30 06:45:32 -0800

    one error fixed

--- app/routes.py
+++ app/routes.py
@@ -150,6 +150,6 @@ def explore():
         if posts.has_next else None
     prev_url = url_for('index', page=posts.prev_num) \
         if posts.has_prev else None
-    return render_template('index.html', title='Explore', posts=posts.items
+    return render_template('index.html', title='Explore', posts=posts.items,
                             next_url=next_url, prev_url=prev_url)
 
